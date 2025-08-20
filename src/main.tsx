878273d4ff625f1980ea42018bdd00c45d9eff5d
Yeshaswi Singh <111686033+yeshaswi3060@users.noreply.github.com>
2025-08-20 10:51:06 -0600

    traffic tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import StreetRoastBot from './StreetRoastBot.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StreetRoastBot />
+    <Analytics />
   </React.StrictMode>,
 )
